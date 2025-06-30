b838f38d7e7e857fcaebcb5dab35dc5be9ffb9ae
jfaa-josh <josh@jfadvancedanalytics.com>
2025-06-30 17:29:11 -0400

    Testing CI and improving streaming code.

--- src/stockops/data/utils.py
+++ src/stockops/data/utils.py
@@ -1,9 +1,15 @@
-def get_db_path(db_date: date | None = None) -> Path:
+from datetime import date
+from pathlib import Path
+
+from stockops.config import DATA_DB_DATESTR, RAW_STREAMING_DIR
+
+
+def get_streaming_db_path(db_date: date | None = None) -> Path:
     """
     Returns the path to the .db file for the given date.
     If no date is given, uses today's date.
     """
     if db_date is None:
         db_date = date.today()
-    db_name = db_date.strftime("%Y-%m-%d") + ".db"
-    return RAW_REALTIME_DIR / db_name
+    db_name = db_date.strftime(DATA_DB_DATESTR) + ".db"
+    return RAW_STREAMING_DIR / db_name
--- tests/test_stock_streamer.py
+++ tests/test_stock_streamer.py
@@ -6,15 +6,10 @@ This is a temporary script file.
 import asyncio
 import sys
 
-import stockops
 from stockops import run_data_pipeline
-
-print("stockops.__file__ =", stockops.__file__)
-
-print(f"type of run_data_pipeline: {type(run_data_pipeline)}")
 
 if __name__ == "__main__":
     if sys.platform.startswith("win"):
         asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
-    asyncio.run(run_data_pipeline())
+    asyncio.run(run_data_pipeline(10, ["SPY"]))
