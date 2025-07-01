fe614c9f50e102cc72b908fcb420ac9b0c01f988
jfaa-josh <josh@jfadvancedanalytics.com>
2025-07-01 16:08:32 -0400

    Bug fix2

--- src/stockops/main.py
+++ src/stockops/main.py
@@ -7,4 +7,7 @@ if __name__ == "__main__":
     if sys.platform.startswith("win"):
         asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
-    asyncio.run(run_data_pipeline(120, ["SPY"]))
+    def data_pipeline(duration: int = 120, tickers: list[str] | None = None):
+        asyncio.run(run_data_pipeline(duration, tickers))
+
+    # data_pipeline(120, ["SPY"])
