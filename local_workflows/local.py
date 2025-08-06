934f2f8a825ed48f96f817a0cd8a149e72e97612
jfaa-josh <josh@jfadvancedanalytics.com>
2025-08-06 15:55:10 -0400

    Prefect and Docker are working prefectly at least for a single flow run execution...now debugging python code

--- local_workflows/local.py
+++ local_workflows/local.py
@@ -1,18 +1,22 @@
 import logging
 from typing import Any, Dict
 import asyncio
+import sys
 
-from prefect import flow, task, get_run_logger
 from stockops.data.historical.providers import get_historical_service
 from stockops.data.streaming.providers import get_streaming_service
 from stockops.data.controller import Controller
 
+# Logging setup
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s | %(levelname)s | %(name)s | %(message)s",
+    handlers=[logging.StreamHandler(sys.stdout)],
+    force=True,
+)
 logger = logging.getLogger(__name__)
 
-
-@task
 def run_controller_task(command: Dict[str, Any], command_type: str, provider: str = "EODHD") -> None:
-    logger = get_run_logger()
     logger.info("Preparing to run controller with type '%s' and command: %s", command_type, command)
 
     try:
@@ -32,13 +36,17 @@ def run_controller_task(command: Dict[str, Any], command_type: str, provider: st
         logger.exception("Controller task failed for command: %s", command)
         raise
 
-@flow(name="controller_driver_flow")
 def controller_driver_flow(
     command: Dict[str, Any],
     command_type: str,
     provider: str = "EODHD"
 ) -> None:
-    logger = get_run_logger()
     logger.info("Running controller_driver_flow...")
 
     run_controller_task(command, command_type, provider)
+
+command = {'ticker': 'SPY.US', 'interval': '1m', 'start': '2025-07-02 09:30', 'end': '2025-07-02 16:00'}
+command_type = 'fetch_historical'
+provider = 'EODHD'
+
+controller_driver_flow(command, command_type, provider)
--- ui/streamlit_app.py
+++ ui/streamlit_app.py
@@ -1,8 +1,9 @@
 import streamlit as st
 import logging
 import sys
+import time
 
-import api_calls as api
+import api_calls
 
 # Logging setup
 logging.basicConfig(
@@ -13,9 +14,13 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
-# Initialize prefect API
-resp = api.register_controller_flow('controller_driver_flow')
-flow_id = resp['id']
+
+# Initialize backend only the first time this user hits the app
+if "initialized" not in st.session_state:
+    st.session_state.api = api_calls.APIBackend("controller_driver")
+    st.session_state.initialized = True
+
+api = st.session_state.api
 
 # UI layout
 st.title("Prefect Trigger Dashboard")
@@ -24,21 +29,33 @@ if st.button("Run Historical SPY"):
     logger.info("Button pressed: Run Historical SPY")
     try:
         deployment_name = 'some-deployment'
-        response = api.register_deployment(flow_id, deployment_name)
+        schedules = None # No schedules = on‐demand runs only; add a list of DeploymentScheduleCreate
+        response = api.register_deployment(deployment_name, schedules)
         logger.info(f"Button pressed, response is: {response}")
+        deployment_id = response['id']
 
+        status = "INITIAL"
+        while status != "READY":
+            depl_response = api.check_deployment_status(deployment_id)
+            status = depl_response.get("status")
+            # print(f"YO MF:  STATUS IS {status}")
+            if status == "NOT_READY":
+                logger.info("Deployment is in NOT_READY status.  Waiting 5 seconds, and checking again...")
 
-        # result = api.trigger_flow(
-        #     command_type="fetch_historical",
-        #     command_payload={
-        #         "ticker": "SPY.US",
-        #         "interval": "1m",
-        #         "start": "2025-07-02 09:30",
-        #         "end": "2025-07-02 16:00"
-        #     },
-        #     provider="EODHD"
-        # )
-        # st.json(result)
+            elif status is None:
+                logger.exception("There is no deployment with this id!")
+            time.sleep(5)
+
+        if status == "READY":
+            ticker = 'SPY.US'
+            interval = '1m'
+            start = '2025-07-02 09:30'
+            end = '2025-07-02 16:00'
+            command_type = 'fetch_historical'
+            provider = 'EODHD'
+            response = api.run_deployed_flow(deployment_id, ticker, interval,
+                                            start, end, command_type, provider)
+            flow_run_name = response['name']
     except Exception as e:
         logger.exception("Failed to trigger historical SPY flow")
         st.error("Flow trigger failed.")
