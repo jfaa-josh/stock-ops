5fa5e2e263ea4cc772f8251899c02cd6e035aa6a
jfaa-josh <josh@jfadvancedanalytics.com>
2025-09-10 11:44:56 -0400

    Bug fix attempt for CI 2

--- src/stockops/config/eodhd_config.py
+++ src/stockops/config/eodhd_config.py
@@ -1,5 +1,8 @@
+import logging
 import os
 from pathlib import Path
+
+logger = logging.getLogger(__name__)
 
 # root level
 ROOT_DIR = Path(__file__).resolve().parents[3]
@@ -11,19 +14,19 @@ if os.getenv("ENV") != "production":
     dotenv_path = ROOT_DIR / ".env"
     load_dotenv(dotenv_path, override=False)
 
-EODHD_API_TOKEN = os.getenv("EODHD_API_TOKEN")
-if not EODHD_API_TOKEN:
-    raise ValueError("EODHD_API_TOKEN not set. Check .env or GitHub Secrets.")
-
-# Exchange metadata
-EXCHANGE_METADATA = {
-    "US": {
-        "Timezone": "America/New_York",
-        "Currency": "USD",
-        "TradingHours": {
-            "Open": "09:30:00",
-            "Close": "16:00:00",
-            "WorkingDays": "Mon,Tue,Wed,Thu,Fri",
-        },
+EODHD_API_TOKEN = os.getenv("EODHD_API_TOKEN") or None
+if EODHD_API_TOKEN:
+    # Exchange metadata
+    EXCHANGE_METADATA = {
+        "US": {
+            "Timezone": "America/New_York",
+            "Currency": "USD",
+            "TradingHours": {
+                "Open": "09:30:00",
+                "Close": "16:00:00",
+                "WorkingDays": "Mon,Tue,Wed,Thu,Fri",
+            },
+        }
     }
-}
+else:
+    logger.info("EODHD_API_TOKEN not set. Check .env or GitHub Secrets if it was intended.")
