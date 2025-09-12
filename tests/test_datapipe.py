9dd5dbd84339e24d4ea6ca5b22c2dcb14ec81190
jfaa-josh <josh@jfadvancedanalytics.com>
2025-09-12 17:23:26 -0400

    CI test

--- tests/test_datapipe.py
+++ tests/test_datapipe.py
@@ -6,10 +6,10 @@ from pathlib import Path
 import pytest
 
 ENTRYPOINTS = [
-    # ("local_workflows.streamlit_local.local_streamlit_ci", "main"),
-    # ("local_workflows.local_ETL", "test_ci"),
+    ("local_workflows.streamlit_local.local_streamlit_ci", "main"),
+    ("local_workflows.local_ETL", "test_ci"),
     ("local_workflows.local_write", "main"),
-    # ("local_workflows.reader_local.local_read_ci", "main"),
+    ("local_workflows.reader_local.local_read_ci", "main"),
 ]
 
 pytestmark = pytest.mark.integration
